7741f37993a8f7d2c28682222debb1f746283efa
Rafal Stapinski <stapinskirafal@gmail.com>
2017-11-05 17:02:30 -0500

    start with easyai

--- ai-testing/client/easy_client.py
+++ ai-testing/client/easy_client.py
@@ -1,18 +1,20 @@
-import easyAI.TwoPlayersGame
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 import json
 
-class AIPlayer(WebSocket):
+class EasyAIPlayer(WebSocket):
+
+    self.clients = []
 
     def handleMessage(self):
         # self.sendMessage(self.data)
         print self.data
 
     def handleConnected(self):
+        # self.clients.append(self.address, )
         print self.address, 'connected'
 
     def handleClose(self):
         print self.address, 'closed'
 
-server = SimpleWebSocketServer('', 8000, AIPlayer)
+server = SimpleWebSocketServer('', 8000, EasyAIPlayer)
 server.serveforever()
